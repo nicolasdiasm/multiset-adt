4e7e9c78424cf732dc058459c8d8363997abfa78
Nicolas Dias Martins <nicolasci2018@gmail.com>
2023-09-18 11:22:45 -0400

    added docs

---
+++ src/LinkedListMultiSet.java
@@ -0,0 +1,11 @@
+/**
+ * Unlike the TreeMultiList, this implementation does not just "wrap" an
+ *         underlying tree, it is instead a custom LinkedListMultiSet implementation, which
+ *         only provides the necessary MultiSet methods.
+ *
+ *         Representation Invariant:
+ *         self._front is None represents an empty MultiSet
+ */
+
+public class LinkedListMultiSet {
+}
