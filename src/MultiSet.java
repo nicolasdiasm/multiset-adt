75eeb9a72179f9453e0163aa3de88372536e4a5d
Arnnav Aggarwal <arnnav.aggarwal@mail.utoronto.ca>
2023-09-18 11:21:11 -0400

    Try adding add method to MultiSet

--- src/MultiSet.java
+++ src/MultiSet.java
@@ -1,6 +1,11 @@
+import jdk.jshell.spi.ExecutionControl;
+
 /**
  * MultiSet ADT
  */
 
 public class MultiSet {
+    public boolean add(Object item) throws ExecutionControl.NotImplementedException {
+        throw new ExecutionControl.NotImplementedException("Accessing parent class");
+    }
 }
