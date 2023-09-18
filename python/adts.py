1157a4861232b6e6a12f373b52a3cad0ff03f2ec
Nicolas Dias Martins <nicolasci2018@gmail.com>
2023-09-18 11:24:16 -0400

    correction

--- python/adts.py
+++ python/adts.py
@@ -513,7 +513,7 @@ class ArrayListMultiSet(MultiSet):
 class LinkedListMultiSet(MultiSet):
     """
     Unlike the TreeMultiList, this implementation does not just "wrap" an
-    underlying tree, it is instead a custom LinkedList implementation, which
+    underlying tree, it is instead a custom LinkedListMultiSet implementation, which
     only provides the necessary MultiSet methods.
 
     Representation Invariant:
