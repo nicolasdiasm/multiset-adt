1e1beb4fdd77a374c5e946078cce0da431931583
Jia Gu <jjia.gu@mail.utoronto.ca>
2023-09-21 20:32:21 -0400

    finished ArrayList

--- src/ArrayListMultiSet.java
+++ src/ArrayListMultiSet.java
@@ -1,2 +1,43 @@
+import java.util.ArrayList;
+
 public class ArrayListMultiSet extends MultiSet {
+
+    private ArrayList<Object> alist;
+
+    public ArrayListMultiSet() {
+        this.alist = new ArrayList<>();
+    }
+
+    public boolean add(Object item) {
+        this.alist.add(item);
+        return true;
+    }
+
+    public void remove(Object item) {
+            this.alist.remove(item);
+    }
+
+    public boolean contains(Object item) {
+        return this.alist.contains(item);
+    }
+
+    public boolean is_empty() {
+       return this.alist.isEmpty();
+    }
+
+    public int count(Object item) {
+        int count = 0;
+        for (Object o : this.alist)
+            if (o == item) {
+                count ++;
+            }
+        return count;
+    }
+
+    public int size() {
+        return this.alist.size();
+    }
 }
+
+
+
