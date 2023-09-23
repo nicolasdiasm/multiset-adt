6949b6a8f5103af4438f83a1fba27b8fb87ae3dd
Nicolas Dias Martins <nicolasci2018@gmail.com>
2023-09-22 21:55:21 -0400

    treemultiset implemented

--- src/TreeMultiSet.java
+++ src/TreeMultiSet.java
@@ -2,4 +2,41 @@
  * This class uses an underlying Tree to implement our MultiSet ADT.
  */
 public class TreeMultiSet extends MultiSet {
+    private Tree tree;
+
+    public TreeMultiSet() {
+        this.tree = null;
+    }
+    @Override
+    public boolean add(Object item) {
+        this.tree.insert(item);
+        return true;
+    }
+
+    @Override
+    public void remove(Object item) {
+        this.tree.deleteItem(item);
+    }
+
+    @Override
+    public boolean contains(Object item) {
+        return this.contains(item);
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return this.tree.isEmpty();
+    }
+
+    @Override
+    public int count(Object item) {
+        return this.tree.count(item);
+    }
+
+    @Override
+    public int size() {
+        return this.tree.len();
+    }
+
 }
+
